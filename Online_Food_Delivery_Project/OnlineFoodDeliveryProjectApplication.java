dadef4ae3c1a6b1b4a385bbd83cc036ff57a3913
pavanjoy2002 <155639042+pavanjoy2002@users.noreply.github.com>
2024-05-27 20:34:25 +0530

    Add files via upload

---
+++ Online_Food_Delivery_Project/OnlineFoodDeliveryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.proj.Online_Food_Delivery_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodDeliveryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodDeliveryProjectApplication.class, args);
+	}
+
+}
