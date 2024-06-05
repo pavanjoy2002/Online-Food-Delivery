b96b2a0dc44fddd51ddc2326ece867a31ab66bf7
pavanjoy2002 <155639042+pavanjoy2002@users.noreply.github.com>
2024-06-05 10:29:37 +0530

    Update Header.js

--- Frontend_Project/src/components/Header/Header.js
+++ Frontend_Project/src/components/Header/Header.js
@@ -1,26 +1,28 @@
-import React from "react";
-const Header = () => {
-  return (
-    <>
-      <nav className="navbar navbar-dark bg-primary ">
-        <div className="container-fluid">
-          <a className="navbar-brand fs-3 px-5">Food ADDA</a>
-          <div className="lg-rg bg-primary mx-2 px-2">
-            <a href="login.html">
-              <button type="button" className="btn btn-outline-light mx-2">
-                Login
-              </button>
-            </a>
-            <a href="register.html">
-              <button type="button" className="btn btn-outline-light mx-2">
-                Register
-              </button>
-            </a>
-          </div>
-        </div>
-      </nav>
-    </>
-  )
-}
+  import React from "react";
+  import { Link } from "react-router-dom";
 
-export default Header;
+  const Header = () => {
+    return (
+      <>
+        <nav className="navbar navbar-dark bg-primary ">
+          <div className="container-fluid">
+            <a className="navbar-brand fs-3 px-5">Food ADDA</a>
+            <div className="lg-rg bg-primary mx-2 px-2">
+              <Link to="/login">
+                <button type="button" className="btn btn-outline-light mx-2">
+                Login 
+                </button>
+              </Link>
+              <Link to="/register">
+                <button type="button" className="btn btn-outline-light mx-2">
+                  Register
+                </button>
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </>
+    )
+  }
+
+  export default Header;
