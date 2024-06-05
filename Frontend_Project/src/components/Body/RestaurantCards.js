19d0ac063d65f6dd3224f91d09ce634a050e9fbc
pavanjoy2002 <155639042+pavanjoy2002@users.noreply.github.com>
2024-06-05 10:28:16 +0530

    Update and rename Cards.js to RestaurantCards.js

--- Frontend_Project/src/components/Body/RestaurantCards.js
+++ Frontend_Project/src/components/Body/RestaurantCards.js
@@ -1,9 +1,9 @@
-const RestaurantCards = () => {
+const RestaurantCards = (props) => {
     return(
         <>
         <div className="cards my-5 mx-5" >
-            <div className="card-group p-3 shadow mt-5">
-                <div className="card shadow mx-2" style="width: 18rem;">
+            {/* <div className="card-group p-3 shadow mt-5"> */}
+                <div className="card shadow mx-2" style={{width: "16rem "}}>
                     <img src="https://cdn.pixabay.com/photo/2022/03/02/12/40/dish-7043065_1280.jpg" className="card-img-top"
                         alt="..."/>
                     <div className="card-body">
@@ -16,7 +16,7 @@ const RestaurantCards = () => {
                         <a href="#" className="btn btn-primary">Add To Cart</a>
                     </div>
                 </div>
-                <div className="card shadow mx-2" style="width: 18rem;">
+                <div className="card shadow mx-2" style={{width: "16rem "}}>
                     <img src="https://cdn.pixabay.com/photo/2022/02/12/15/00/biryani-7009110_1280.jpg"
                         className="card-img-top" alt="..."/>
                     <div className="card-body">
@@ -29,7 +29,7 @@ const RestaurantCards = () => {
                         <a href="#" className="btn btn-primary">Add To Cart</a>
                     </div>
                 </div>
-                <div className="card shadow mx-2" style="width: 18rem;">
+                <div className="card shadow mx-2" style={{width: "16rem "}}>
                     <img src="https://cdn.pixabay.com/photo/2022/03/02/12/40/dish-7043065_1280.jpg" className="card-img-top"
                         alt="..."/>
                     <div className="card-body">
@@ -42,7 +42,7 @@ const RestaurantCards = () => {
                         <a href="#" className="btn btn-primary">Add To Cart</a>
                     </div>
                 </div>
-                <div className="card shadow mx-2" style="width: 18rem;">
+                <div className="card shadow mx-2" style={{width: "16rem "}}>
                     <img src="https://cdn.pixabay.com/photo/2022/02/12/15/00/biryani-7009110_1280.jpg"
                         className="card-img-top" alt="..."/>
                     <div className="card-body">
@@ -55,7 +55,20 @@ const RestaurantCards = () => {
                         <a href="#" className="btn btn-primary">Add To Cart</a>
                     </div>
                 </div>
-                <div className="card shadow mx-2" style="width: 18rem;">
+                <div className="card shadow mx-2" style={{width: "16rem "}}>
+                    <img src="https://cdn.pixabay.com/photo/2022/03/02/12/40/dish-7043065_1280.jpg" className="card-img-top"
+                        alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Pot Biryani</h5>
+                        <p className="card-text">Hotel MunthaTaj</p>
+                        <p className="card-text">Biryani</p>
+                        <p className="card-text">Rating : 4.0</p>
+                        <p className="card-text">Price : Rs.299/-</p>
+                        <p className="card-text">Delivery Time : 25-35 Min</p>
+                        <a href="#" className="btn btn-primary">Add To Cart</a>
+                    </div>
+                </div>
+                <div className="card shadow mx-2" style={{width: "16rem "}}>
                     <img src="https://cdn.pixabay.com/photo/2022/02/12/15/00/biryani-7009110_1280.jpg"
                         className="card-img-top" alt="..."/>
                     <div className="card-body">
@@ -68,9 +81,23 @@ const RestaurantCards = () => {
                         <a href="#" className="btn btn-primary">Add To Cart</a>
                     </div>
                 </div>
-            </div>
+                <div className="card shadow mx-2" style={{width: "16rem "}}>
+                    <img src="https://cdn.pixabay.com/photo/2022/03/02/12/40/dish-7043065_1280.jpg" className="card-img-top"
+                        alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Pot Biryani</h5>
+                        <p className="card-text">Hotel MunthaTaj</p>
+                        <p className="card-text">Biryani</p>
+                        <p className="card-text">Rating : 4.0</p>
+                        <p className="card-text">Price : Rs.299/-</p>
+                        <p className="card-text">Delivery Time : 25-35 Min</p>
+                        <a href="#" className="btn btn-primary">Add To Cart</a>
+                    </div>
+                </div>
+            {/* </div> */}
         </div>
         </>
     )
 }
-export default RestaurantCards  ;
+
+export default RestaurantCards;
