2cfe33d6bc8fbff0d610df6e809e05c4cfeb2764
pavanjoy2002 <155639042+pavanjoy2002@users.noreply.github.com>
2024-06-05 10:31:15 +0530

    Update app.js

--- Frontend_Project/app.js
+++ Frontend_Project/app.js
@@ -1,23 +1,86 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Header from './src/components/Header/Header';
+import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import Images from './src/components/Images/image';
+import Header from "./src/components/Header/Header";
 import Body from "./src/components/Body/Body";
 import Footer from "./src/components/Footer/Footer";
+import ShimmerEffect from "./src/components/ShimmerEffect/ShimmerEffect";
+import { RouterProvider,Outlet, createBrowserRouter } from "react-router-dom";
+import Error from "./src/components/Error";
+import Login from "./src/components/Body/Login";
+import UserRegister from "./src/components/Body/UserRegister";
+import AdminHeader from "./src/components/Header/AdminHeader";
 
 
 var htmlRoot = document.getElementById("root");
 var RootReact = ReactDOM.createRoot(htmlRoot);
 
-const Run = () => {
-  return (
+const App = () => {
+  return(
     <>
-      <Header/>
-      <Body/>
-      <Footer/>
+    {/* <ShimmerEffect/> */}
+     <Header/>
+     <Outlet/>
+     <Footer/>
     </>
-  );
-};
+  )
+}
 
-RootReact.render(<Run />);
+const Admin = () => {
+  return(
+    <>
+      <AdminHeader/>
+      <Outlet/>
+    </>
+  )
+}
+
+const Welcome = () => {
+  return(
+    <>
+    <Body/>
+    </>
+  )
+}
+const appRouter = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element: <App/>,
+      children:[
+        {
+          path:"/welcome",
+          element:<Welcome/>,
+        },
+        {
+          path:"/login",
+          element:<Login/>,
+        },
+        {
+          path:"/register",
+          element:<UserRegister/>
+        },
+        {
+          path:"/login/welcome",
+          element:<Welcome/>,
+        }
+      ],
+      errorElement:<Error/>
+    },
+    {
+      path:"/admin",
+      element:<AdminHeader/>,
+      children:[
+        {
+
+        }
+      ]
+    }
+  ]
+)
+// RootReact.render(<App />);
+
+RootReact.render(<RouterProvider router={appRouter}/>)
+
+// ReactDOM.render(<App />, document.getElementById("root"));
