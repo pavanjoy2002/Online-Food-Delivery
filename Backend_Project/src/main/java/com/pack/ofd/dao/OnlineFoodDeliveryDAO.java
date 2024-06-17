91b19b3ef75682a1f4f14d89faeb93da06087c95
pavanjoy2002 <155639042+pavanjoy2002@users.noreply.github.com>
2024-06-17 08:16:49 +0530

    Update OnlineFoodDeliveryDAO.java

--- Backend_Project/src/main/java/com/pack/ofd/dao/OnlineFoodDeliveryDAO.java
+++ Backend_Project/src/main/java/com/pack/ofd/dao/OnlineFoodDeliveryDAO.java
@@ -10,18 +10,18 @@ import com.pack.ofd.model.User;
 
 public class OnlineFoodDeliveryDAO {
 
-	public boolean validatingUser(String email, String pswd) throws SQLException {
+	public boolean validatingUser(String fromTable, String email, String pswd) throws SQLException {
 		User user = new User();
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3306/Online_Food_Delivery","root","root");
 			Statement st = con.createStatement();
-			String loginValidateQuery = "SELECT email_id, password FROM user WHERE email_id = '"
+			String loginValidateQuery = "SELECT email_id, password FROM "+ fromTable +" WHERE email_id = '"
 					+ email + "&& password ='" + pswd
 					+ "'";
 			ResultSet rs = st.executeQuery(loginValidateQuery);
 			while (rs.next()) {
-				System.out.println("Login Successfull !!");
+				System.out.println(" Login Successfull !!");
 				return true;
 			}
 			con.close();
