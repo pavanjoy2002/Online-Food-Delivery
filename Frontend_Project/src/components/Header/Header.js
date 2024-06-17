55197b14c073cbfdaa1fcf2b364fb53fc3ceb9c7
pavanjoy2002 <155639042+pavanjoy2002@users.noreply.github.com>
2024-06-17 08:30:39 +0530

    Update Header.js

--- Frontend_Project/src/components/Header/Header.js
+++ Frontend_Project/src/components/Header/Header.js
@@ -8,15 +8,14 @@
           <div className="container-fluid">
             <a className="navbar-brand fs-3 px-5">Food ADDA</a>
             <div className="lg-rg bg-primary mx-2 px-2">
-              <Link to="/login">
-                <button type="button" className="btn btn-outline-light mx-2">
-                Login 
-                </button>
+              <Link to="/welcome/resregister" className="btn btn-outline-light mx-2">
+                Restaurant Register 
               </Link>
-              <Link to="/register">
-                <button type="button" className="btn btn-outline-light mx-2">
-                  Register
-                </button>
+              <Link to="/welcome/register" className="btn btn-outline-light mx-2">
+                Customer Register
+              </Link>
+              <Link to="/welcome/login" className="btn btn-outline-light mx-2">
+                Login 
               </Link>
             </div>
           </div>
