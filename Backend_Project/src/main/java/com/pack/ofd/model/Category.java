e7bfdefc5a1aba87ada5df0da88225aaf0a1adf0
pavanjoy2002 <155639042+pavanjoy2002@users.noreply.github.com>
2024-06-17 08:17:45 +0530

    Update Category.java

--- Backend_Project/src/main/java/com/pack/ofd/model/Category.java
+++ Backend_Project/src/main/java/com/pack/ofd/model/Category.java
@@ -10,7 +10,9 @@ public class Category {
 	@Id
 	@GeneratedValue
 	private int categoryId;
+	private String categoryImg;
 	private String name;
+	private String description;
 	
 	public int getCategoryId() {
 		return categoryId;
@@ -18,12 +20,23 @@ public class Category {
 	public void setCategoryId(int categoryId) {
 		this.categoryId = categoryId;
 	}
+	public String getCategoryImg() {
+		return categoryImg;
+	}
+	public void setCategoryImg(String categoryImg) {
+		this.categoryImg = categoryImg;
+	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	
 }
