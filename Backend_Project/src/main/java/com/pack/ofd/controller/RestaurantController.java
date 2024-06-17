c28a294a5db6af067c6dbc1103a2e00df629eeac
pavanjoy2002 <155639042+pavanjoy2002@users.noreply.github.com>
2024-06-17 08:15:55 +0530

    Update RestaurantController.java

--- Backend_Project/src/main/java/com/pack/ofd/controller/RestaurantController.java
+++ Backend_Project/src/main/java/com/pack/ofd/controller/RestaurantController.java
@@ -3,35 +3,43 @@ package com.pack.ofd.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pack.ofd.exception.FoodNotFoundException;
+import com.pack.ofd.exception.RestaurantNotFoundException;
 import com.pack.ofd.model.Restaurant;
 import com.pack.ofd.model.User;
 import com.pack.ofd.repository.RestaurantRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:1234")
 public class RestaurantController {
 
 	@Autowired
 	private RestaurantRepository restaurantRepository;
 	
+	@PostMapping("/resregister")
+	Restaurant newRestaurant(@RequestBody Restaurant newRestaurant) {
+		return restaurantRepository.save(newRestaurant);
+	}
 
-//	@PostMapping("/register")
-//	User newUser(@RequestBody User newUser) {
-//		return userRepository.save(newUser);
-//	}
+	@GetMapping("/allrestaurants")
+	List<Restaurant> getAllRestaurants(){
+		return restaurantRepository.findAll();
+	}
 	
-	
-//	@PostMapping("/resregister")
-//	Restaurant newRestaurant(@RequestBody Restaurant newRestaurant) {
-//		return RestaurantRepository.save(newRestaurant);
-//	}
-//	
-//	@GetMapping
-//	List<Restaurant> getAllUsers(){
-//		return restaurantController.findAll();
-//	}
+	@DeleteMapping("/delrestaurant/{id}")
+	String deleteRestaurant(@PathVariable("id") int id) {
+		if(!restaurantRepository.existsById(id)) {
+			throw new RestaurantNotFoundException(id);
+		}
+		restaurantRepository.deleteById(id);
+		return "Restaurant with id :  " +id +" has been deleted successfully.";
+	}
 }
