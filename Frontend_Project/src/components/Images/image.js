d229e4b857083eed9e254cd701ba676a7ca675da
pavanjoy2002 <155639042+pavanjoy2002@users.noreply.github.com>
2024-06-17 08:30:58 +0530

    Delete Frontend_Project/src/components/Images directory

--- Frontend_Project/src/components/Images/image.js
+++
@@ -1,5 +0,0 @@
-const image1 = "https://www.baltana.com/files/wallpapers-2/Food-HD-Pictures-04863.jpg";
-const image2 = "https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-const image3 = "https://th-i.thgim.com/public/incoming/vricut/article65937264.ece/alternates/LANDSCAPE_1200/Spicy%20Venue-apricot%20delight.jpg";
-
-export {image1, image2, image3};
